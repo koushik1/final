560fd26eacf959c09288bd60470c67e33b2de3df
kshanka2 <kshanka2@ncsu.edu>
2020-11-17 16:23:15 -0500

    test lock

--- csc501-lab3/sys/task1.c
+++ csc501-lab3/sys/task1.c
@@ -55,66 +55,69 @@ void testsem()
 	kprintf("\n");
 	kprintf("Priority of A:%d\n", getprio(A));
 				
-}		
-
-
-void lreader (char *msg, int lck)
-{
-        int     ret;
-
-        kprintf ("  %s: to acquire lock\n", msg);
-        lock (lck, READ, 20);
-        kprintf ("  %s: acquired lock\n", msg);
-        kprintf ("  %s: to release lock\n", msg);
-        releaseall (1, lck);
 }
 
-void lwriter (char *msg, int lck)
-{
-        kprintf ("  %s: to acquire lock\n", msg);
-        lock (lck, WRITE, 20);
-        kprintf ("  %s: acquired lock, sleep 10s\n", msg);
-        sleep (10);
-        kprintf ("  %s: to release lock\n", msg);
-        releaseall (1, lck);
+void lock1(int l){
+        kprintf("Process A (writer) waiting for lock\n");
+        lock (l, WRITE, 10);
+        kprintf("Process A (writer) acquires lock\n");
+        sleep(7);
+        kprintf("Process A (writer) releases lock\n");
+        releaseall (1, l);
 }
+
+void lock2(int l){
+        kprintf("Process B (reader) waiting for lock\n");
+        lock (l, READ, 10);
+        kprintf("Process B (reader) acquires lock\n");
+        kprintf("Process B (reader) releases lock\n");
+        releaseall (1, l);
+}
+
+void lock3(int l){
+        kprintf("Process B (reader) waiting for lock\n");
+        lock (l, READ, 10);
+        kprintf("Process B (reader) acquires lock\n");
+        kprintf("Process B (reader) releases lock\n");
+        releaseall (1, l);
+}
+
+
 
 void testlck()
 {
 	int ld;
-	int rd1, rd2;
-	int wr1;
-	
+	int A, B, C;	
 	ld = lcreate(); 
 	
-	rd1 = create(lreader, 2000, 40, "readerA", 2, "reader A", ld);
-        rd2 = create(lreader, 2000, 30, "readerB", 2, "reader B", ld);
-        wr1 = create(lwriter, 2000, 10, "writer", 2, "writer", ld);
+	A = create(lock1, 2000, 10, "A", 1, ld);
+        B = create(lock2, 2000, 20, "B", 1, ld);
+        C = create(lock3, 2000, 30, "C", 1, ld);
 	
-	kprintf("-start writer, then sleep 1s. lock granted to write (prio 10)\n");
-        resume(wr1);
+	kprintf("Start process A (writer) with priority 10\n");
+        resume(A);
         sleep (1);
-	kprintf("Priority of writer:%d\n", getprio(wr1));
+	kprintf("Priority of A:%d\n", getprio(A));
 
-        kprintf("-start reader A, then sleep 1s. reader A(prio 40) blocked on the lock\n");
-        resume(rd1);
+        kprintf("Start process B (reader) with priority 20\n");
+        resume(B);
         sleep (1);
-	kprintf("Priority of writer:%d\n", getprio(wr1));
 
-        kprintf("-start reader B, then sleep 1s. reader B(prio 30) blocked on the lock\n");
-        resume (rd2);
+        kprintf("Start process C (reader) with priority 30\n");
+        resume (C);
 	sleep (1);
-	kprintf("Priority of writer:%d\n", getprio(wr1));
+	kprintf("\n");
+	kprintf("Priority of A:%d\n", getprio(A));
 				
 }
 
 int task1()
 {
-	kprintf("\nTest Results from Semaphore approach :-\n");
+	kprintf("\nSemaphore :-\n");
 	testsem();
-	kprintf("\nEnd of Test Result from Semaphore approach\n");
-	kprintf("\nTest Results from Priority Inheritance approach :-\n");
+	kprintf("\nSemahore Complete\n");
+	kprintf("\nLocks with Priority Inheritence :-\n");
 	testlck();
-	kprintf("\nEnd of Test Result Priority Inheritance approach\n");
+	kprintf("\nLocks with Priority Inheritence Compplete\n");
 	
 }
